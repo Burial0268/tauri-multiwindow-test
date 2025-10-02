f2e98927cf35251fa72c40b80ba5337528416d46
Burial0268 <Burial0268@foxmail.com>
2025-10-03 01:28:29 +0800

    first commit

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,31 @@
+use tauri::{Emitter, Manager};
+use tauri::Url;
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str, handle: tauri::AppHandle) -> String {
+    let url = "https://login.live.com/oauth20_authorize.srf?client_id=aaaf3f8c-8b99-4e7b-8265-b637bd89317e&response_type=code&redirect_uri=http://localhost:8080&scope=XboxLive.signin%20offline_access&state=NOT_NEEDED";
+    let url = Url::parse(url).unwrap();
+    let webview_window = tauri::webview::WebviewWindowBuilder::new(&handle.clone(), "ms", tauri::WebviewUrl::External(url))
+        .on_navigation(move |url| {
+        if url.to_string().contains("code=") {
+            let _ = handle.emit("oauth:ms:callback_url", url.to_string());
+            if let Some(w) = handle.webview_windows().get("ms") {
+                let _ = w.close();
+            }
+        }
+        true
+    })
+    .build()
+    .unwrap();
+    format!("Hello {name}, You have been greeted from Rust!")
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
